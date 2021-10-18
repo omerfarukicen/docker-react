1659795a9eae3c6954de94b2237a2d8d4f92511a
omerfarukicen <omerfarukicen@gmail.com>
2021-10-18 22:42:02 +0300

    new project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <p>DENEME</p>
+        <p>
+          Edit <code>src/App.js</code> *-*-*-*-*
+        </p>
+       
+      </header>
+      Sağolasın2323
+    </div>
+  );
+}
+
+export default App;
